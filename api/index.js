c3968922f9d1aaef89662d8eaeba072fb7fdb912
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-20 22:33:49 -0300

    chore: update shareCode to TIBM0HY0ZJN

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=XVXFAWE60HG&afp_campaign=tapi2' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=TIBM0HY0ZJN&afp_campaign=tapi2' });
     res.end();
   }
