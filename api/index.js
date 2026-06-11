0519d7928c3fb9b6cb1fb0282ca0a2ba466933eb
JCPrime01 <vanzokiller@gmail.com>
2026-06-10 23:08:14 -0300

    chore: shareCode 5RHG8MGEVNF -> PBQCEIRRXT7 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=5RHG8MGEVNF&afp_campaign=ta' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=PBQCEIRRXT7&afp_campaign=ta' });
     res.end();
   }
